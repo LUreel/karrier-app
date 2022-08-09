827654f558563f7f79e7d6814b10118624a25dc3
uritmb@hotmail.com <uritmb@hotmail.com>
2022-08-09 20:47:58 +0200

    a

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -14,8 +14,8 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.DDISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands('1006616701946822706', '607271025104781410'), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
